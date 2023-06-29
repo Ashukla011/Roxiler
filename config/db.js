66e07b054ee8008c46daf912707056f96064ee62
Ashukla011 <shuklavashu9696@gmail.com>
2023-06-29 13:39:33 +0530

    roxiler

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 
 const mongoose=require("mongoose")
-// require("dotenv").config()
+require("dotenv").config()
 
 
 const  connection=mongoose.connect("mongodb+srv://kavanish011:super30shukla@cluster0.cqhfocy.mongodb.net/Roxiler?retryWrites=true&w=majority")
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require("express");
 const connection=require('./config/db.js');
 const app=express();
 app.use(express.json())
-
+require("dotenv").config();
 const {ProductRoutes} =require("../BackEnd/Routes/Product.Routes.js")
 
 
@@ -18,7 +18,7 @@ app.use("/",ProductRoutes)
 
 
 
-app.listen(3500,async()=>{
+app.listen(process.env.port,async()=>{
    try{
     await connection;
     console.log('server is running on 3500')
